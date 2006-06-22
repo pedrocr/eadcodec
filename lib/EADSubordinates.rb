0a6691a7a8ff3bfe957339f01e5f3cb89945c560
Pedro Côrte-Real <pedro@pedrocr.net>
2006-06-22 11:08:43 +0000

    Slightly improve the testing of the partial export and replace tabs with spaces in subordinates.

--- lib/EADSubordinates.rb
+++ lib/EADSubordinates.rb
@@ -1,29 +1,29 @@
 require "EAD.rb"
 
 module EAD
-	class EADSubordinates < EADElement
-		elname "dsc"
-	
-		include EADLevels
-		
-		xmlattr :altrender
-		xmlattr :audience
-		xmlattr :encondinganalog
-		xmlattr :id
-		xmlattr :othertype
-		xmlattr :tpattern
-		xmlattr :type
-		
-		xmlsubel :head
-		xmlsubel_mult :p
-		xmlsubel_mult :c
+  class EADSubordinates < EADElement
+    elname "dsc"
+  
+    include EADLevels
+    
+    xmlattr :altrender
+    xmlattr :audience
+    xmlattr :encondinganalog
+    xmlattr :id
+    xmlattr :othertype
+    xmlattr :tpattern
+    xmlattr :type
+    
+    xmlsubel :head
+    xmlsubel_mult :p
+    xmlsubel_mult :c
 
-		def description_level
-			0
-		end
+    def description_level
+      0
+    end
 
-		def unnumbered?
-			false
-		end
-	end
+    def unnumbered?
+      false
+    end
+  end
 end
--- test/TestPartialExport.rb
+++ test/TestPartialExport.rb
@@ -24,21 +24,27 @@ class TestPartialExport < TestEAD
     c01.did = EAD::EADDescription.new
     c01.did.head = "abc"
     c01.did.unitid = "teste"
+    c01.did.unittitle = "teste"
     
     dsc.c << c01
-    c01.partial_export(file)
+    c01.start_partial_export(file)
     
-    c01 = EAD::EADLevel.new
-    c01.did = EAD::EADDescription.new
-    c01.did.head = "abc"
-    c01.did.unitid = "teste"
+    dsc = EAD::EADSubordinates.new
+    dsc.type = @type
+    c01.dsc << dsc
+    
+    c02 = EAD::EADLevel.new
+    c02.did = EAD::EADDescription.new
+    c02.did.head = "abc"
+    c02.did.unitid = "teste"
+    c02.did.unittitle = "teste"
     sc = EAD::EADScopeContent.new
     sc.p << EAD::EADP.new('value1')
     sc.p << EAD::EADP.new('value2')
-    c01.scopecontent = sc
+    c02.scopecontent = sc
     
-    dsc.c << c01
-    c01.partial_export(file)
+    dsc.c << c02
+    c02.start_partial_export(file)
     
     @ead.end_partial_export(file)
     
@@ -47,7 +53,9 @@ class TestPartialExport < TestEAD
     validate_dtd
     compare_xpath("fonds", "/ead/archdesc/@level")
     compare_xpath(@type, "/ead/archdesc/dsc/@type")
-    element_exists("/ead/archdesc/dsc/c01", "Check that the level was added")
-    element_exists("/ead/archdesc/dsc/c01/did", "Check that the description was added")
+    element_exists("/ead/archdesc/dsc/c01", "the first level was not added")
+    element_exists("/ead/archdesc/dsc/c01/did", "the first level description was not added")
+    element_exists("/ead/archdesc/dsc/c01/dsc/c01", "the second level was not added")
+    element_exists("/ead/archdesc/dsc/c01/dsc/c01/did", "the second level description was not added")
   end
 end
